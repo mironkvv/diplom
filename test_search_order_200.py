35f1dc267ed57464864bce09c2f757cd1f57a4a9
mironkvv <151390198+mironkvv@users.noreply.github.com>
2023-11-18 19:36:11 +0300

    my commit

---
+++ test_search_order_200.py
@@ -0,0 +1,5 @@
+#Мирослав Кольцов 10 когорта дипломный проект инженер по тестированию плюс
+import Create_and_Search_Order
+
+def test_search_order_status_code_200():
+    assert Create_and_Search_Order.search_order().status_code == 200
