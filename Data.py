32c06e17b667000cdf78e6512c5c737be7782299
mironkvv <151390198+mironkvv@users.noreply.github.com>
2023-11-18 19:36:34 +0300

    my commit

---
+++ Data.py
@@ -0,0 +1,13 @@
+User_body = {
+    "firstName": "Miroslav",
+    "lastName": "Koltsov",
+    "address": "SPB",
+    "metroStation": 4,
+    "phone": "+7 800 800 80 80",
+    "rentTime": 5,
+    "deliveryDate": "2023-10-10",
+    "comment": "Hello",
+    "color": [
+        "BLACK"
+    ]
+}
